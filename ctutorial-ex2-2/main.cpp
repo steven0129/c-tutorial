5e1aa7bc60fc36136962667a4f65b0d869196fab
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-05-17 23:21:54 +0800

    init commit

--- ctutorial-ex2-2/main.cpp
+++ ctutorial-ex2-2/main.cpp
@@ -1,7 +1,26 @@
-#include <cstdio>
+#include<stdio.h>
+#include<float.h>
 
 int main()
 {
-    printf("hello from ctutorial_ex2_2!\n");
-    return 0;
+	puts("\nCharacteristics of the type float\n");
+
+	printf("Storage size: %d bytes\n"
+		"Smallest positive value: %E\n"
+		"Greatest positive value: %E\n"
+		"Precision: %d decimal digits\n",
+		sizeof(float), FLT_MIN, FLT_MAX, FLT_DIG);
+
+	puts("\nAn example of float precision:\n");
+	double d_var = 12345.6;
+	float f_var = (float)d_var;
+	printf("The floating-point number"
+		"%18.10f\n", d_var);
+
+	printf("has been stored in a variable of type float as the value %18.10f\n", f_var);
+
+	printf("The rouding error is"
+		"%18.10f\n", d_var - f_var);
+
+	return 0;
 }
